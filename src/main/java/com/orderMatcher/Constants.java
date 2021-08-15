52b41605b7f85a9a8b6ed6b362b373fa9c3d6b33
KIRAN <ramkirand@gmail.com>
2021-08-15 19:54:26 +0530

    Cleaned up

--- src/main/java/com/orderMatcher/Constants.java
+++ src/main/java/com/orderMatcher/Constants.java
@@ -1,10 +1,8 @@
 package com.orderMatcher;
 
 public class Constants {
-	public static final String SPACE = " ";
-	public static final String AT = "@";
-	public static final int INITIAL_CAPACITY = 5;
-	public static final String TRADE = "Trade";
-	public static final String DISPLAY_SELL_ORDER_MIN_HEAP = "Display sellOrderMinHeap:";
-	public static final String DISPLAY_BUY_ORDER_MAX_HEAP = "Display buyOrderMaxHeap:";
+    public static final String SPACE = " ";
+    public static final String AT = "@";
+    public static final int INITIAL_CAPACITY = 15;
+    public static final String TRADE = "Trade";
 }
